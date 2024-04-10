1a2865f3f694817748085d538ed0ca8e6b7fc9d9
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-10 17:57:40 +0200

    Added header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,12 @@
 <template>
     <header>
-        <div id="header-container"  class="container">
+        <div id="header-container"  class="container d-flex align-content-center align-items-center">
             <div id="left-navbar">
                 <img src="../assets/img/dc-logo.png" alt="Logo">
             </div>
             <div id="rigth-navbar">
-                <ul>
-                    <li v-for="(item, index) in navbar" :key="index"><a href="item.url"></a>{{ item.name }}</li>
+                <ul class="d-flex justify-content-between">
+                    <li v-for="(item, index) in navbar" :key="index"><a class="text-uppercase" href="item.url">{{ item.name }}</a></li>
                 </ul>
 
             </div>
@@ -70,6 +70,7 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
 
 header{
     width: 100%;
@@ -77,8 +78,33 @@ header{
     #header-container{
         margin: 0 auto;
         border: 1px solid red;
+        display: flex;
+
     }
-  
+
+    #left-navbar{
+        width: 30%;
+    }
+
+    #rigth-navbar{
+        width: 70%;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    a{
+        text-decoration: none;
+        color: $black;
+        font-size: 0.8em;
+
+        &:hover{
+            color: $bluemain;
+            border-bottom: 4px solid $bluemain;
+            padding-bottom: 45px;
+        }
+    }
 }
 
 </style>
