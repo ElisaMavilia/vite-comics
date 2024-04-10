57a524e12802ec42f7b3d1af9918bccf6c626737
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-10 17:35:14 +0200

    added navbar via v-for

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,16 @@
 <template>
     <header>
         <div id="header-container"  class="container">
-            <img src="../assets/img/dc-logo.png" alt="Logo">
+            <div id="left-navbar">
+                <img src="../assets/img/dc-logo.png" alt="Logo">
+            </div>
+            <div id="rigth-navbar">
+                <ul>
+                    <li v-for="(item, index) in navbar" :key="index"><a href="item.url"></a>{{ item.name }}</li>
+                </ul>
+
+            </div>
+            
         </div>
         
     </header>
@@ -12,6 +21,48 @@
         name: 'HeaderComponent',
         data(){
             return{
+                navbar: [
+                    {
+                        name: 'charachters',
+                        url: '#'
+                    },
+                    {
+                        name: 'comics',
+                        url: '#'
+                    },
+                    {
+                        name: 'movies',
+                        url: '#'
+                    },
+                    {
+                        name: 'tv',
+                        url: '#'
+                    },
+                    {
+                        name: 'games',
+                        url: '#'
+                    },
+                    {
+                        name: 'collectibles',
+                        url: '#'
+                    },
+                    {
+                        name: 'videos',
+                        url: '#'
+                    },
+                    {
+                        name: 'fans',
+                        url: '#'
+                    },
+                    {
+                        name: 'news',
+                        url: '#'
+                    },
+                    {
+                        name: 'shop',
+                        url: '#'
+                    },
+                ]
 
             }
         }
